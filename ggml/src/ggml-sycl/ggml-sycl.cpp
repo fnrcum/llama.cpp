8241e1a83a24293ee1f1f8a4abc9e45928e0f741
hmscider <hmscider@users.noreply.github.com>
2026-07-03 07:30:03 -0400

    PR-25222 revision v2: addressed audits

--- ggml/src/ggml-sycl/ggml-sycl.cpp
+++ ggml/src/ggml-sycl/ggml-sycl.cpp
@@ -346,6 +346,7 @@ static void ggml_check_sycl() try {
         GGML_LOG_INFO("  GGML_SYCL_FA_ONEDNN: %d\n", g_ggml_sycl_fa_onednn);
 #else
         GGML_LOG_INFO("  GGML_SYCL_DISABLE_DNN: DNN disabled by compile flag\n");
+        GGML_LOG_INFO("  GGML_SYCL_FA_ONEDNN: %d\n", g_ggml_sycl_fa_onednn);
 #endif
 #if defined(GGML_SYCL_USE_VMM)
         GGML_LOG_INFO("  GGML_SYCL_ENABLE_VMM: %d\n", g_ggml_sycl_enable_vmm);
